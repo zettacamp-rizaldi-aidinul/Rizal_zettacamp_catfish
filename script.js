fd9d69e2b06982ce184043b689e6adeee0d70b9f
Rizaldi Aidinul <114064023+zettacamp-rizaldi-aidinul@users.noreply.github.com>
2022-10-12 15:08:26 +0700

    final answer

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
 let book = {
     title : "Cinderella",
-    price : 120000,
-    stock : 30
+    price : 100000,
+    stock : 30,
+    amount : 20
 }
 
 const discount = 0.05;
 const tax = 0.1;
+const termOfCredit = 7;
 
-function answer(book, discount, tax) {
-    amount = 30
+function answer(book, discount, tax, termOfCredit) {
     let priceBook = 0;
-    let sisa = 0
-    if (amount <= book.stock) {
-        for (i = 1; i <= amount; i++) {
-            priceBook = priceBook + book.price;
-            sisa = book.stock-amount;
+    let months = 1;
+    let sisa = 0;
+    for (i = 1; i <= book.stock; i++) {
+        priceBook = priceBook + book.price;
+        if (book.amount > 0) {
+            book.amount--;
+            sisa = book.stock - i;
         }
-    }
-    else {
-        priceBook = book.price * book.stock;
-        sisa = book.stock-amount;
+        else {
+            break;
+        }
     }
     let totalDiscount = priceBook * discount;
     let totalTax = (priceBook - totalDiscount) * tax;
-    let resultPrice = {
-        priceDiscount : totalDiscount,
-        priceAfterDiscount : priceBook - totalDiscount,
-        priceTax : totalTax,
-        totalPrice : priceBook - totalDiscount + totalTax,
-        sisaBuku : sisa
+    let priceAfterDiscount = priceBook - totalDiscount;
+    let priceTax = totalTax;
+    let totalPrice = priceBook - totalDiscount + totalTax;
+    let credit = []
+    for (j = 0; j < termOfCredit; j++) {
+        credit.push({
+            "month" : months + j, 
+            "creditPrice" : totalPrice / termOfCredit,
+            "paylater" : (totalPrice - ((totalPrice / termOfCredit) * (j + 1)))
+        })
     }   
-    return resultPrice;
+    
+    document.getElementById("sisa").innerHTML = sisa;
+    document.getElementById("price-discount").innerHTML = priceAfterDiscount;
+    document.getElementById("price-tax").innerHTML = priceTax;
+    document.getElementById("total-price").innerHTML = totalPrice;
+
+    return credit;
 }
 
-const detail = answer(book, discount, tax);
+const detail = answer(book, discount, tax, termOfCredit);
 
+for (j = 0; j <= termOfCredit; j++) {
+    console.log(detail[j]);
+}
 document.getElementById("title").innerHTML = book.title;
 document.getElementById("price").innerHTML = book.price;
 document.getElementById("stock").innerHTML = book.stock;
-document.getElementById("sisa").innerHTML = detail.sisaBuku;
-document.getElementById("price-discount").innerHTML = detail.priceAfterDiscount;
-document.getElementById("price-tax").innerHTML = detail.priceTax;
-document.getElementById("total-price").innerHTML = detail.totalPrice;
+
+for (i = 0; i <= termOfCredit; i++ ) {
+    var li = document.createElement("li");
+    li.style.listStyle = "none";
+    var textli = document.createTextNode( 'Bulan ke- : ' + detail[i].month + ' Cicilan : ' + detail[i].creditPrice + ' Sisa Pembayaran : ' + detail[i].paylater);
+    li.appendChild(textli);
+     
+    document.getElementById("showlist").appendChild(li);
+    }
